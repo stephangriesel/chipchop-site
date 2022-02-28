1e3c5f908b9f908a44dd7fb9fed144b64731c325
Stephan <sgriesel@gmail.com>
2022-02-28 06:51:24 +0100

    update to dynamic content

--- src/components/Home/InfoCompany2.js
+++ src/components/Home/InfoCompany2.js
@@ -2,34 +2,61 @@ import React from 'react'
 import styled from 'styled-components'
 import { motion } from "framer-motion";
 import { InView } from 'react-intersection-observer';
+import { StaticQuery, graphql } from 'gatsby'
+
+const getAboutWhy = graphql`
+{
+    allContentfulAbout {
+    edges {
+      node {
+        whyBlock3 {
+          internal {
+            content
+          }
+        }
+        whyBlock4 {
+          internal {
+            content
+          }
+        }
+      }
+    }
+  }
+  }
+`
 
 export default function Info() {
     return (
-        <Background>
-            <section className="py-5 info-section">
-                <div className="container"></div>
-                <div className="row">
-                    <InView threshold={0.25} triggerOnce>
-                        {({ ref, inView }) => (
-                            <motion.div
-                                className="col-10 col-sm-8 mx-auto text-center"
-                                ref={ref}
-                                initial={{ opacity: 0 }}
-                                animate={inView ? { opacity: 1 } : { opacity: 0 }}
-                                transition={{ duration: 0.6 }}
-                            >
-                                <p className="lead text-muted mb-5">
-                                    Chipchop is not for the cheap. Chipchop is for those who want to have some fun while designing their interior. For those who want imaginative furniture that really celebrates colour, contrast, texture, material and space. And even for those who do not yet know what they want.
-                                </p>
-                                <p className="lead text-muted mb-5">
-                                    We can provide you with design insights, inspiring ideas and project visualisations, all based on your own space and personal taste. We help you make up your mind, custom-make your durable furniture and make sure you get what your interior needs in the end, always with a smile.
-                                </p>
-                            </motion.div>
-                        )}
-                    </InView>
-                </div>
-            </section>
-        </Background>
+        <StaticQuery query={getAboutWhy} render={data => {
+            // console.log("test data:", data)
+            return (
+                <Background>
+                    <section className="py-5 info-section">
+                        <div className="container"></div>
+                        <div className="row">
+                            <InView threshold={0.25} triggerOnce>
+                                {({ ref, inView }) => (
+                                    <motion.div
+                                        className="col-10 col-sm-8 mx-auto text-center"
+                                        ref={ref}
+                                        initial={{ opacity: 0 }}
+                                        animate={inView ? { opacity: 1 } : { opacity: 0 }}
+                                        transition={{ duration: 0.6 }}
+                                    >
+                                        <p className="lead text-muted mb-5">
+                                            {data.allContentfulAbout.edges[0].node.whyBlock3.internal.content}
+                                        </p>
+                                        <p className="lead text-muted mb-5">
+                                            {data.allContentfulAbout.edges[0].node.whyBlock4.internal.content}
+                                        </p>
+                                    </motion.div>
+                                )}
+                            </InView>
+                        </div>
+                    </section>
+                </Background>
+            )
+        }} />
     )
 }
 
