30b530b7788782e685cda66ba234484125f2a1d9
Stephan <sgriesel@gmail.com>
2022-04-10 15:39:41 +0200

    add swiper

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -22,6 +22,11 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
       integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
       crossOrigin="anonymous"
       defer
+    />,
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>,
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
     />
   ])
 }
