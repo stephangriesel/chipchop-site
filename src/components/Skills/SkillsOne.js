45fe8b54a947a26c9b42414789e57d551e7ce355
Stephan <sgriesel@gmail.com>
2022-04-11 07:54:12 +0200

    update images

--- src/components/Skills/SkillsOne.js
+++ src/components/Skills/SkillsOne.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { useScroll } from '../useScroll';
 import { fade } from '../../animation';
-import img from '../../images/projects/project-nieuw-west.jpg'
+import img1 from '../../images/projects/project-nieuw-west1.jpg'
+import img2 from '../../images/projects/project-nieuw-west2.jpg'
+import img3 from '../../images/projects/project-nieuw-west3.jpg'
 
 const SkillsOne = () => {
     const [element, controls] = useScroll();
@@ -20,13 +22,13 @@ const SkillsOne = () => {
                 animate={controls}
             >
                 {/* Slider main container */}
-                {/* <div class="swiper"> */}
+                {/* <div className="swiper"> */}
                     {/* Additional required wrapper */}
-                    {/* <div class="swiper-wrapper"> */}
+                    {/* <div className="swiper-wrapper"> */}
                         {/* Slides */}
-                        {/* <div class="swiper-slide"> */}
+                        {/* <div className="swiper-slide"> */}
                             <img
-                                src={img}
+                                src={img1}
                                 alt='installation'
                                 placeholder='tracedSVG'
                                 width={600}
@@ -37,11 +39,11 @@ const SkillsOne = () => {
                     {/* <div class="swiper-pagination"></div> */}
 
                     {/* If we need navigation buttons */}
-                    {/* <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div> */}
+                    {/* <div className="swiper-button-prev"></div>
+                    <div className="swiper-button-next"></div> */}
 
                     {/* If we need scrollbar */}
-                    {/* <div class="swiper-scrollbar"></div> */}
+                    {/* <div className="swiper-scrollbar"></div> */}
                 {/* </div> */}
             </motion.div>
         </div>
